396b1894955134bfed68a5408fa59a3a3ced1263
somidotun <106350605+somidotun@users.noreply.github.com>
2026-05-15 21:09:41 +0100

    fix : resolve conversation conflict

--- src/store/signupStore.ts
+++ src/store/signupStore.ts
@@ -22,7 +22,7 @@ interface SignupStore {
 
 export const useSignupStore = create<SignupStore>((set) => ({
   isLoading: false,
-  isSuccess: false, // change it back to false after testing
+  isSuccess: false,
   serverError: null,
   focusedFields: {},
   formData: null,
