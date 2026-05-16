42bcc6a0416197e60f8ffc13d8112010044704e2
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-05-16 15:01:45 +0100

    fix: adjust desktop preview layout and optimize images with Next.js Image

--- src/components/common/LandingPage/herroSection.tsx
+++ src/components/common/LandingPage/herroSection.tsx
@@ -87,8 +87,8 @@ export default function HeroSection() {
               <Link
                 href="/watch-demo"
                 className="flex justify-center items-center w-86.25 h-14 px-4 py-2.5 
-                mx-auto bg-[#F7F9FB] border border-[#DADADA] text-[#0F172A] 
-                font-semibold rounded-lg text-[16px] hover:bg-[#02505E] 
+                  mx-auto bg-[#F7F9FB] border border-[#DADADA] text-[#0F172A] 
+                  font-semibold rounded-lg text-[16px] hover:bg-[#02505E] 
                 hover:text-[#F7F9FB] transition-colors gap-2 cursor-pointer"
               >
                 Watch Demo
