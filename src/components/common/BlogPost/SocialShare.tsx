4f9bf5b245160fbfa73451f7eceaf098788620f6
Amarkin <akinolamartins05@gmail.com>
2026-05-18 08:27:22 +0100

    fix: update blog page and social share component

--- src/components/common/BlogPost/SocialShare.tsx
+++ src/components/common/BlogPost/SocialShare.tsx
@@ -1,8 +1,13 @@
+<<<<<<< Updated upstream
 import { FaFacebookF, FaTwitter, FaPinterest } from "react-icons/fa";
+=======
+ import { FaFacebookF, FaTwitter, FaPinterest } from 'react-icons/fa';
+>>>>>>> Stashed changes
 
 export default function SocialShare() {
   return (
     <div className="flex items-center w-full my-10">
+<<<<<<< Updated upstream
 
       {/* Facebook */}
       <button
@@ -18,10 +23,14 @@ export default function SocialShare() {
           cursor-pointer
         "
       >
+=======
+      <button className="flex-1 flex items-center justify-center gap-2 py-8 border-t border-b-2 border-t-gray-100 border-b-[#3b5998] text-[#3b5998] hover:bg-gray-50 transition-colors text-sm font-medi
+>>>>>>> Stashed changes
         <FaFacebookF className="size-5" />
         <span>Share 694</span>
       </button>
 
+<<<<<<< Updated upstream
       {/* Twitter */}
       <button
         className="
@@ -35,10 +44,14 @@ export default function SocialShare() {
           cursor-pointer
         "
       >
+=======
+      <button className="flex-1 flex items-center justify-center gap-2 py-8 border-t border-b-2 border-t-gray-100 border-b-sky-400 text-sky-500 hover:bg-gray-50 transition-colors text-sm font-medium u
+>>>>>>> Stashed changes
         <FaTwitter className="size-5" />
         <span>Tweet</span>
       </button>
 
+<<<<<<< Updated upstream
       {/* Pinterest */}
       <button
         className="
@@ -59,3 +72,12 @@ export default function SocialShare() {
     </div>
   );
 }
+=======
+      <button className="flex-1 flex items-center justify-center gap-2 py-8 border-t border-b-2 border-t-gray-100 border-b-red-600 text-gray-500 hover:bg-gray-50 transition-colors text-sm font-medium 
+        <FaPinterest className="text-red-600 size-5" />
+        <span>694</span>
+      </button>
+    </div>
+  );
+}
+>>>>>>> Stashed changes
